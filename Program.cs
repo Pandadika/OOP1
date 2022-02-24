3a4d9f788c00f1fefed6597074fc6c8548ccef38
pandadika <pandadika@gmail.com>
2022-02-24 12:36:49 +0100

    init boxes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOP1Opgave1.codes;
+
+
+Box lilleBox = new Box();
+Box lilleBox2 = new Box(2.3,2.5,2.2, BoxType.lilleBox);
+Box toSmåBoxes = lilleBox + lilleBox2;
+Box mediumBox = new Box(2,3,4,BoxType.mediumBox);
+Box storBox = mediumBox + toSmåBoxes;
+
+List<Box> boxes = new() { lilleBox, lilleBox2, toSmåBoxes, mediumBox, storBox };
+
+foreach (var box in boxes)
+{
+    Console.WriteLine(box.ToString());
+}
+
+Console.WriteLine("\nBox sorteret efter volume");
+
+boxes.Sort();
+foreach (var box in boxes)
+{
+    Console.WriteLine("Volume: " + box.GetVolume()+ " " +box.ToString());
+}
---
+++ codes/Box.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1Opgave1.codes
+{
+    public enum BoxType
+    {
+        lilleBox,
+        mediumBox,
+        storBox
+    }
+    public class Box : IComparable<Box>
+    {
+        public double H { get;}
+        public double W { get;}
+        public double D { get;}
+        public BoxType BoxType { get;}
+
+        public Box(double h, double w, double d, BoxType boxType) {
+            H = h;
+            W = w;
+            D = d;
+            BoxType = boxType;
+        }
+        public Box() { 
+            H = 1.0;
+            W = 1.0;
+            D = 1.0;
+            BoxType = BoxType.lilleBox;
+        }
+        public double GetVolume()
+        {
+            return H * W * D;
+        }
+
+        public override string ToString()
+        {
+            string type = "";
+            switch (BoxType)
+            {
+                case BoxType.lilleBox: type = "lille box";
+                    break;
+                case BoxType.mediumBox: type = "mellem box";
+                    break;                
+                case BoxType.storBox: type = "stor box";
+                    break;
+            }
+             
+            return $"Højde = {H}, Længde = {W}, Dybde = {D}, Type = {type}";
+        }
+
+        public int CompareTo(Box? other)
+        {
+            if (other.GetVolume() > this.GetVolume()) return -1;
+            if (other.GetVolume() == this.GetVolume()) return 0;
+            return 1;
+        }
+
+        public static Box operator+(Box a, Box b)
+        {
+            BoxType boxType;
+            if (a.BoxType==BoxType.lilleBox && b.BoxType == BoxType.lilleBox)
+            {
+                boxType = BoxType.mediumBox;
+            }
+            else boxType = BoxType.storBox;
+            Box result = new(a.H+b.H,a.W+b.W,a.D+b.D, boxType);
+            return result;
+        }
+
+
+
+    }
+}
