97d665b48fa7d5dacba62b1cbdde8267a79c2594
pandadika <pandadika@gmail.com>
2022-02-24 11:58:05 +0100

    done

--- Program.cs
+++ Program.cs
@@ -3,100 +3,37 @@ using OOP1.codes;
 using OOP1.codes.models;
 
 
-List<Student> Students = new(){
-    new() { Id = 1, FirstName = "Martin", LastName = "Jensen"},
-    new() { Id = 2, FirstName = "Patrik", LastName = "Nielsen" },
-    new() { Id = 3, FirstName = "Susanne", LastName = "Hansen" },
-    new() { Id = 4, FirstName = "Thomas", LastName = "Olsen" },
-};
-List<Teacher> Teachers = new(){
-    new() { Id = 1, FirstName = "Niels", LastName = "Olesen" },
-    new() { Id = 2, FirstName = "Henrik", LastName = "Paulsensen" }
-};
 List<Course> Courses = new(){
     new() { Id = 1, CourseName = "Grundliggende programmering" , TeacherId = 1},
     new() { Id = 2, CourseName = "Database programmering", TeacherId = 1},
     new() { Id = 3, CourseName = "Studieteknik", TeacherId = 1},
     new() { Id = 4, CourseName = "Clientside programmering", TeacherId = 2}
 };
-List<Enrollment> Enrollments = new();
 
-
-
-//School s = new();
 Semester semester = new("H1", "TEC");
 
-string? output;
-string input;
 
-
-System.Console.WriteLine("Angiv Skole");
-input = Console.ReadLine();
-while(input != semester.GetSchoolName()){
-    System.Console.WriteLine("Skole findes ikke");
-    System.Console.WriteLine("Angiv Skole");
-    input = Console.ReadLine();
-}
-output = input+", ";
-System.Console.WriteLine("Angiv Hovedforløb");
-input = Console.ReadLine();
-while(input != semester.GetSemesterName()){
-    System.Console.WriteLine("Hovedforløb findes ikke");
-    System.Console.WriteLine("Angiv Hovedforløb");
-    input = Console.ReadLine();
-}
-output += input+" ";
-output += "Fag tilmeldings app.";
-System.Console.WriteLine(output);
-
-System.Console.WriteLine("Indtast elev");
-input = Console.ReadLine();
-int inputId;
-
-try
-{
-    inputId = Int32.Parse(input);
-}
-catch (System.Exception)
-{
-    System.Console.WriteLine("Student findes ikke");
-    throw;
-}
-
-int StudentId = 0;
-foreach (var student in Students)
-{
-    if (student.Id == inputId)
-    {
-        StudentId = inputId;
-    }
-}
-
-
-System.Console.WriteLine("Indtast Course");
-input = Console.ReadLine();
-try
-{
-    inputId = Int32.Parse(input);
-}
-catch (System.Exception e)
-{
-    System.Console.WriteLine("Course findes ikke");
-    throw;
-}
-int CourseId = 0;
+Console.WriteLine("Usorteret Liste\n------------------------");
 foreach (var course in Courses)
 {
-    if (course.Id == inputId)
-    {
-        CourseId = inputId;
-    }
+    Console.WriteLine($"{course.Id}: {course.CourseName}");
 }
 
-Enrollments.Add(new Enrollment() { Id = Enrollments.Count()+1, CourseId = CourseId, StudentId = StudentId});
-
-foreach (var enrollment in Enrollments)
+Console.WriteLine("\nAlfabetisk sorteret\n------------------------");
+Courses.Sort();
+foreach (var course in Courses)
 {
-    System.Console.WriteLine(enrollment.ToString());
+    Console.WriteLine($"{course.Id}: {course.CourseName}");
 }
-
+Courses.Reverse();
+Console.WriteLine("\nReverse Alfabetisk sorteret\n------------------------");
+foreach (var course in Courses)
+{
+    Console.WriteLine($"{course.Id}: {course.CourseName}");
+}
+Console.WriteLine("\nICompare Id sorteret\n------------------------");
+Courses.Sort(new CourseIdComperer());
+foreach (var course in Courses)
+{
+    Console.WriteLine($"{course.Id}: {course.CourseName}");
+}
--- codes/models/Course.cs
+++ codes/models/Course.cs
@@ -3,11 +3,25 @@ using System;
 namespace OOP1.codes.models
 
 {
-    internal class Course
+    internal class Course : IComparable<Course>
     {
         public int Id { get; set; }
-        public string CourseName { get; set; }
+        public string? CourseName { get; set; }
         public int TeacherId { get; set; }
+
+        public int CompareTo(Course other)
+        {
+            return CourseName.CompareTo(other.CourseName);
+        }
+
+    }
+
+    class CourseIdComperer : IComparer<Course>
+    {
+        public int Compare(Course? x, Course? y)
+        {
+            return x.Id.CompareTo(y.Id);
+        }
     }
 
 }
